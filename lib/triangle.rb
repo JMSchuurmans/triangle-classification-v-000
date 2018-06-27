1baee79d3c50755214f84a60e7c5a624bb431ec3
Jeremy Schuurmans <jeremyschuurmans@gmail.com>
2018-06-27 18:18:59 +0000

    automatically backed up by learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -18,7 +18,12 @@ class Triangle
     end
   end
 
-  def 
+  def triangle_checker
+    valid_triangle = [(side_a + side_b > side_c), (side_a + side_c > side_b), (side_b + side_c > side_a)]
+    [side_a, side_b, side_c].each {|side| valid_triangle << false if side <= 0}
+    raise TriangleError if valid_triangle.include?(false)
+  end
+    
 
 
   class TriangleError < StandardError
