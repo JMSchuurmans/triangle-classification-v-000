44aac306792b1c2a8d6a4c5121eb9ed017d417d5
Jeremy Schuurmans <jeremyschuurmans@gmail.com>
2018-06-27 18:14:20 +0000

    automatically backed up by learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,29 @@
 class Triangle
-  # write code here
+
+  attr_reader :side_a, :side_b, :side_c
+
+  def initialize(side_a, side_b, side_c)
+    @side_a = side_a
+    @side_b = side_b
+    @side_c = side_c
+  end
+
+  def kind
+    if side_a == side_b && side_a == side_c
+      return :equilateral
+    elsif side_a == side_b || side_a == side_c || side_b == side_c
+      return :isosceles
+    else
+      return :scalene
+    end
+  end
+
+  def 
+
+
+  class TriangleError < StandardError
+    def message
+      "A triangle with no size is not a triangle."
+    end
+  end
 end
